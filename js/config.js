82c71a8dc65a40d5f2b27b208047e23534822381
Jamie Gamblin <jamiemgamblin@gmail.com>
2017-07-25 23:00:03 -0600

    new api keys

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export default {
     marvelEndPoint: 'http://gateway.marvel.com/v1/public/characters',
-    privateKey: '830a520a4f95bbcd9491ba4ff952d03afb40f7e4',
-    publicKey: '46f9107d375f7fe73a40a3431b71ab94',
+    privateKey: '6d87e2d611560d4e27ea4faf54e2547bcced5ff1',
+    publicKey: 'd54d5ead523fd3131f28c881265f4cf6',
 }
